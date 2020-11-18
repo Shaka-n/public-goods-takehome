adfad40cb044338b6b1ab506e94ee4f6593a202e
Garen Sahagian <g.s.sahagian@gmail.com>
2020-11-18 14:00:50 -0500

    Struggling with the animation.

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 // Un-hides the popup after 5 seconds
 document.addEventListener("DOMContentLoaded", ()=>{
-    setTimeout(()=> document.getElementById('popup-box').classList.remove('hide'), 5000)
+    setTimeout(()=> document.getElementById('popup-box').classList.remove('hide').classList.add('show'), 5000)
+})
+// 
+
+document.addEventListener('click', (e)=>{
+    if(e.target.textContent==="X" || e.target.textContent==='Claim Now'){
+        document.getElementById('popup-box').classList.add('hide')
+        document.getElementById('popup-box').classList.remove('show')
+    }
 })
