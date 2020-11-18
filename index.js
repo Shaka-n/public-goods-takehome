71def473625ef8c6763e4992ea55518b2e9bd8d0
Garen Sahagian <g.s.sahagian@gmail.com>
2020-11-18 13:47:38 -0500

    roughing out popup

---
+++ index.js
@@ -0,0 +1,4 @@
+// Un-hides the popup after 5 seconds
+document.addEventListener("DOMContentLoaded", ()=>{
+    setTimeout(()=> document.getElementById('popup-box').classList.remove('hide'), 5000)
+})
